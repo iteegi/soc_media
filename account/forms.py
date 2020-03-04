9ea19f1d9f786eb3ad9c402ab3abc2f0921bc4a8
J D <jd@gmail.com>
2020-03-05 02:07:54 +0300

    added user registration form
    created a new user registration handler
    
    added a template for user registration page in urls.py
    
    Added register.html file
    
    added register_done.html file
    
    added a link to the registration page in the login template

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 """Forms for the accounts."""
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -7,3 +8,24 @@ class LoginForm(forms.Form):
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    """User registration form."""
+
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        """Meta information."""
+
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        """Check if both passwords match."""
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают.')
+        return cd['password2']
--- account/views.py
+++ account/views.py
@@ -5,7 +5,28 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    """Register user."""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
 
 
 @login_required
